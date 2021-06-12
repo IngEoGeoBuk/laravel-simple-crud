914e5b09728e8e6410c7427202f997db2d0095a0
IngEoGeoBuk <you3667@naver.com>
2021-06-12 17:23:12 +0900

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All Post</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+<body>
+    <section style="padding-top:60px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            All Post <a href="/add-post" class="btn btn-success">Add new Post</a>
+                        </div>
+                        <div class="card-body">
+                            @if(Session::has('post_deleted'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ Session::get('post_deleted') }}
+                                </div>
+                            @endif
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Post Title</th>
+                                        <th>Post Description</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($posts as $post)
+                                        <tr>
+                                            <td>{{ $post->id }}</td>
+                                            <td>{{ $post->title }}</td>
+                                            <td>{{ $post->body }}</td>
+                                            <td>
+                                                <a href="/posts/{{$post->id}}" class="btn btn-info">
+                                                    Details
+                                                </a>
+                                                <a href="/edit-post/{{$post->id}}" class="btn btn-success">
+                                                    Edit
+                                                </a>
+                                                <a href="/delete-post/{{$post->id}}" class="btn btn-danger">
+                                                    Delete
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+</html>
